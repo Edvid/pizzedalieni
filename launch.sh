d1bf6c47855605853fbb68af5cef915019ffc21b
Edvid <spacemastr@gmail.com>
2024-02-17 20:01:00 +0100

    better way of dealing with .env file in launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 docker-compose down -t 0
-export $(cat .env | xargs) && echo NEXT_PUBLIC_API_URL=$API_URL >> ./website/.env.local
+source ./.env
+echo NEXT_PUBLIC_API_URL=$API_URL >> ./website/.env.local
 docker-compose up -d --build
