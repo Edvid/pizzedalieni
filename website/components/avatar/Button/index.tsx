a8040bacfb3ec2826e9cb504a243c5d9beffd8f1
Edvid <spacemastr@gmail.com>
2024-03-13 14:21:48 +0100

    Removed unused import

--- website/components/avatar/Button/index.tsx
+++ website/components/avatar/Button/index.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 import { userState } from '@/utils/userStates'
 import getCookie from '@/utils/getCookie'
 
