cdea1df1d28c124721ae605635c4b1e7542b578e
Edvid <spacemastr@gmail.com>
2024-03-27 02:30:07 +0100

    Better Accept Cookie banner

--- website/components/cookieAccept.tsx
+++ website/components/cookieAccept.tsx
@@ -1,6 +1,7 @@
 'use client'
 import getCookiePermission from "@/utils/cookie/getCookiePermission";
 import setCookiePermission from "@/utils/cookie/setCookiePermission";
+import DetailedDescription from "@/components/detailedDescription";
 import { useState } from "react"
 
 export default function CookieAccept () {
@@ -10,15 +11,31 @@ export default function CookieAccept () {
     setCookiePermission();
     setShow(false);
   }
+  
+  const technicalCookieReasons = [
+    "BasketContent - stores what you have in your basket as a cookie so it won't be forgotten when you reload the site",
+    "token - An encrypted cookie containing info about which user is logged in which only my server knows how to read. Essential for staying logged in as you navigate the site or authorizing any user specific behaviour on the site",
+    "cookie-permission - A cookie that can only ever be set to \"yes\". Only used for knowing if or if you haven't accepted these cookies being used. No deny option, you can only ignore. Remembering your choice to deny would mean storing a cookie. Can't have that. The cookie permission box will have to follow you around everywhere as you use the site."
+  ];
 
   if(show) {
     return (
-      <div className="m-2 p-8 fixed z-20 bg-zinc-500 rounded-lg w-60">
+      <div className="m-2 p-8 fixed z-20 bg-zinc-500 rounded-lg w-[40rem]">
         <p>
-          henlo, pls accept cok
+          Hello! This website is a portfolio project for me to land a job. The site uses a few cookies for essential functionality. None of them are tracking cookies. If you do not accept, we won't go behind your back and make use of the necessary cookies anyway. We will respect your wish to not store or read cookies on your browser and our site will loose functionality.
         </p>
+        <DetailedDescription title="Technical Description">
+          <p>
+            There's only {technicalCookieReasons.length} cookies:
+          </p>
+          <ul>
+          {technicalCookieReasons.map((text: string, i: number) => (
+            <li key={i} className="pt-3">{text}</li>
+          ))}
+          </ul>
+        </DetailedDescription>
         <button className="bg-green-500 my-2 p-4 rounded-lg" onClick={acceptedCookie}>
-          Pls
+          Accept Necessary
         </button>
       </div>
 
---
+++ website/components/detailedDescription/index.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode, useState } from 'react'
+import styles from './styles.module.scss';
+
+interface IDetailedDescription {
+  title: string; 
+  children: ReactNode
+}
+
+function InfoIcon () {
+  return (
+    <div className='h-[10px] bg-blue-400 rounded-xl'>
+      <p className={styles["material-symbols-outlined"] + " " + styles["info"]}>info</p>
+    </div>
+  )
+}
+
+export default function DetailedDescription(props: IDetailedDescription) {
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(!show);
+  }
+
+  return (
+    <div className="pt-4">
+      <button className='inline-flex' onClick={handleClick}>
+        <p className='underline decoration-dashed underline-offset-4 cursor-pointer'>{props.title}</p>
+        <InfoIcon/>
+        <p className={styles["material-symbols-outlined"]}>arrow_drop_{show ? "up" : "down"}</p>
+      </button>
+      <div className='my-4 ml-6'>
+        {show ? props.children : ""}
+      </div>
+    </div>
+  )
+}
