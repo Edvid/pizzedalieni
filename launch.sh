4f60316ba39ed973dc0ad1b531494088c7c642f9
Edvid <spacemastr@gmail.com>
2024-04-01 03:45:44 +0200

    docker compose command generalized
    docker-compose changed in favour of docker-compose in both launch
    files to have it be understood by the version on the raspberry pi.

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
-docker-compose down -t 0
+docker compose down -t 0
 source ./.env
 echo NEXT_PUBLIC_API_URL=$API_URL >> ./website/.env.local
-docker-compose up -d --build
+docker compose up -d --build
