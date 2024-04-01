c9abb572631f62bbe7100ac4db204612914e0196
Edvid <spacemastr@gmail.com>
2024-04-01 16:44:28 +0200

    Silly cleanup in .env to .env.local

--- launch-dev.sh
+++ launch-dev.sh
@@ -1,6 +1,6 @@
 docker compose down -t 0
 source ./.env
-echo NEXT_PUBLIC_API_URL=$API_URL >> ./website/.env.local
+echo NEXT_PUBLIC_API_URL=$API_URL > ./website/.env.local
 docker compose -f docker-compose-dev.yaml up -d --build
 cd website
 npm run dev
