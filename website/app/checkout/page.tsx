e35bca5ad42968102b1b3ae0ff825a68039be004
Edvid <spacemastr@gmail.com>
2024-05-09 03:07:50 +0200

    BoughtTable into its own component

--- website/app/checkout/page.tsx
+++ website/app/checkout/page.tsx
@@ -1,20 +1,8 @@
-"use client"
 import NavBar from '@/components/navBar'
 import styles from './styles.module.css';
-import setCookie from '@/utils/cookie/setCookie';
-import getCookie from '@/utils/cookie/getCookie';
-import deleteCookie from '@/utils/cookie/deleteCookie';
-import postPizzas from '@/utils/api/postPizzas';
+import BoughtTable from '@/components/boughtTable';
 
 export default function Checkout () {
-
-  const currentBasket = getCookie("current-basket");
-
-  if(typeof currentBasket !== 'undefined' && currentBasket !== '[]'){
-    deleteCookie("current-basket");
-    postPizzas();
-    setCookie("bought-basket", currentBasket ?? "[]");
-  }
 
   return (
     <main>
@@ -28,53 +16,7 @@ export default function Checkout () {
         <p>
           Your order is
         </p>
-          <table className='m-auto rounded-lg'>
-          <tbody>
-            <tr>
-              <th className="p-4 border-2 border-transparent border-b-white">
-                Pizza number
-              </th>
-              <th className="p-4 border-2 border-transparent border-b-white">
-                Name
-              </th>
-              <th className="p-4 border-2 border-transparent border-b-white">
-                Amount
-              </th>
-              <th className="p-4 border-2 border-transparent border-b-white">
-                Price
-              </th>
-            </tr>
-            {
-              JSON.parse(getCookie("bought-basket") ?? "[]")
-              .map((
-                element: {
-                  itemId: number,
-                  name: string,
-                  price: string,
-                  amount: string
-                },
-                i: number
-              ) => (
-                  <tr key={i}>
-                    <td className='p-4'>
-                      {element.itemId}
-                    </td>
-                    <td className="p-4">
-                      {element.name}
-                    </td>
-                    <td className="p-4">
-                      {element.amount}
-                    </td>
-                    <td className="p-4">
-                      {element.price.slice(1)}Ƶ x {element.amount}
-                      <br/>
-                      {(Number(element.price.slice(1)) * Number(element.amount)).toFixed(2)}Ƶ
-                    </td>
-                  </tr>
-                ))
-            }
-          </tbody>
-        </table>
+        <BoughtTable/>
         <p>
           Your order will arrive in approxiately 4000 earth years, give or take
           2 lunar cycles. You pay when we arrive. Be sure to be ready to answer
